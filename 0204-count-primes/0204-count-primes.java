f63daa615bc2d918659808fee1541bdef27f0271
Karan Sharma <ks7585805@gmail.com>
2026-07-27 09:46:29 +0530

    Time: 84 ms (93.18%), Space: 51.1 MB (54.77%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public int countPrimes(int n) {
+//         int count = 0;
+//         boolean[] notPrime = new boolean[n];
+//         for(int i = 2;i*i< n;i++){
+//             if(!notPrime[i]){
+//                for(int j = i; i*j < n;j++){
+//                 notPrime[i*j] = true;
+//                }
+//             }
+//         }
+//         for(int i = 2;i < notPrime.length;i++){
+//             if(!notPrime[i]){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
 class Solution {
     public int countPrimes(int n) {
+        if(n<=2) return 0;
+        boolean[] isNotPrime = new boolean[n];
         int count = 0;
-        boolean[] notPrime = new boolean[n];
-        for(int i = 2;i*i< n;i++){
-            if(!notPrime[i]){
-               for(int j = i; i*j < n;j++){
-                notPrime[i*j] = true;
-               }
+        for(int i=2;i*i<n;i++){
+            if(!isNotPrime[i]){
+                for(int j = i*i; j<n; j+=i){
+                    isNotPrime[j] = true;
+                }
             }
         }
-        for(int i = 2;i < notPrime.length;i++){
-            if(!notPrime[i]){
+        for (int i = 2; i < n; i++) {
+            if (!isNotPrime[i]) {
                 count++;
             }
         }
+
         return count;
     }
 }

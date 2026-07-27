c0b23e1343d4f28dc722984ab788a4b38e3d21a5
Karan Sharma <ks7585805@gmail.com>
2026-07-27 11:33:06 +0530

    Time: 4 ms (50.31%), Space: 67 MB (58.42%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -1,26 +1,23 @@
 // class Solution {
 //     public int[][] construct2DArray(int[] original, int m, int n) {
-//         int[][] ans = new int[0][0];
+//         if (m * n != original.length) return new int[0][0];
 //         int k = 0;
-//         if(m*n < original.length || m*n > original.length)return ans;
-//         int[][] ans1 = new int[m][n];
+//         int[][] ans = new int[m][n];
 //         for(int i = 0; i < m;i++){
 //             for(int j = 0;j <n;j++){
-//                 ans1[i][j] = original[k++];
+//                 ans[i][j] = original[k++];
 //             }
-//         } 
-//         return ans1;
+//         }
+//         return ans;
 //     }
 // }
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
         if (m * n != original.length) return new int[0][0];
-        int k = 0;
+
         int[][] ans = new int[m][n];
-        for(int i = 0; i < m;i++){
-            for(int j = 0;j <n;j++){
-                ans[i][j] = original[k++];
-            }
+        for (int k = 0; k < original.length; k++) {
+            ans[k / n][k % n] = original[k];
         }
         return ans;
     }

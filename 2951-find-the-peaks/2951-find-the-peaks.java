3c5624fcacb19a6c0927077e0aa30847cb6c1e56
Karan Sharma <ks7585805@gmail.com>
2026-07-30 14:36:21 +0530

    Time: 1 ms (99.64%), Space: 45.6 MB (35.14%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.java
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -1,12 +1,25 @@
+// class Solution {
+//     public List<Integer> findPeaks(int[] mountain) {
+//         List <Integer> list =new ArrayList<>();
+//         int k = 0;
+//         for(int i = 1;i < mountain.length -1;i++){
+//             if(mountain[i] > mountain[i - 1] && mountain[i] > mountain[i+1]){
+//                 list.add(i);
+//             }
+//         }
+//         return list;
+//     }
+// }
 class Solution {
-    public List<Integer> findPeaks(int[] mountain) {
-        List <Integer> list =new ArrayList<>();
-        int k = 0;
-        for(int i = 1;i < mountain.length -1;i++){
-            if(mountain[i] > mountain[i - 1] && mountain[i] > mountain[i+1]){
-                list.add(i);
+    public List<Integer> findPeaks(int[] m) {
+        List<Integer> a=new ArrayList<>();
+        for (int i=1;i<m.length-1;i++){
+            int num=m[i];
+            if(num>m[i-1] && num>m[i+1]){
+                a.add(i);
+                i++;
             }
         }
-        return list;
+        return a;
     }
 }

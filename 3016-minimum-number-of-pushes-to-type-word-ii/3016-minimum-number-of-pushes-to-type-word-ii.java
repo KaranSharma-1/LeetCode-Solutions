a50e1ac8d6f3ae2852ecfd27f5616dd3dbb68dd8
Karan Sharma <ks7585805@gmail.com>
2026-07-30 09:44:26 +0530

    Time: 8 ms (99.71%), Space: 47.9 MB (73.84%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -15,23 +15,41 @@
 //         return ans;
 //     }
 // }
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int[] freq = new int[26];
+//         for (char c : word.toCharArray()) {
+//             freq[c - 'a']++;
+//         }
+
+//         Arrays.sort(freq);
+
+//         int totalPushes = 0;
+//         int rank = 0;
+
+//         for (int i = 25; i >= 0 && freq[i] > 0; i--, rank++) {
+//             int pushCost = (rank / 8) + 1;
+//             totalPushes += pushCost * freq[i];
+//         }
+
+//         return totalPushes;
+//     }
+// }
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
-        for (char c : word.toCharArray()) {
-            freq[c - 'a']++;
+        int[]arr=new int[26];
+        for(int i=0;i<word.length();i++){
+            char ch=word.charAt(i);
+            arr[ch-'a']++;
         }
-
-        Arrays.sort(freq);
-
-        int totalPushes = 0;
-        int rank = 0;
-
-        for (int i = 25; i >= 0 && freq[i] > 0; i--, rank++) {
-            int pushCost = (rank / 8) + 1;
-            totalPushes += pushCost * freq[i];
+        Arrays.sort(arr);
+        int c=0;
+        int ans=0;
+        for(int i=25;i>=0;i--){
+            int t=(c/8)+1;
+            ans+=t*arr[i];
+            c++;
         }
-
-        return totalPushes;
+        return ans;
     }
 }

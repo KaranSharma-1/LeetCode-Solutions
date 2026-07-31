614f70ac0fca7008615084a2d9c17547b4e96e12
Karan Sharma <ks7585805@gmail.com>
2026-07-31 10:49:08 +0530

    Time: 7 ms (37.52%), Space: 47.9 MB (6.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,15 +1,31 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int max = 0;
+//         HashSet<Character> set = new HashSet<>();
+//         int j = 0;
+//         for(int i = 0 ;i < s.length();i++){
+//             while(set.contains(s.charAt(i))){
+//                     set.remove(s.charAt(j));
+//                     j++;
+//             }
+//             set.add(s.charAt(i));
+//             max = max > i - j + 1 ? max : i - j + 1 ;
+//         }
+//         return max;
+//     }
+// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        HashSet<Character> set = new HashSet<>();
-        int j = 0;
-        for(int i = 0 ;i < s.length();i++){
-            while(set.contains(s.charAt(i))){
-                    set.remove(s.charAt(j));
-                    j++;
+        int[] lastSeen = new int[128];
+        Arrays.fill(lastSeen, -1);
+        int j = 0, max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (lastSeen[c] != -1 && lastSeen[c] >= j) {
+                j = lastSeen[c] + 1;
             }
-            set.add(s.charAt(i));
-            max = max > i - j + 1 ? max : i - j + 1 ;
+            lastSeen[c] = i;
+            max = Math.max(max, i - j + 1);
         }
         return max;
     }

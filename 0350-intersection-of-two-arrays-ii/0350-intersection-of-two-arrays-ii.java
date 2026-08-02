f50e0e45981b3d299db9c2bb284bc156a5a99835
Karan Sharma <ks7585805@gmail.com>
2026-08-02 20:48:49 +0530

    Time: 0 ms (100%), Space: 44.7 MB (85.46%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,27 +1,52 @@
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+//         if(nums1.length <= 0 ||nums2.length <= 0 ){
+//             return new int[0];
+//         }
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         int[]ans = new int[nums1.length];
+//         int k = 0;
+//         int i = 0;
+//         int j = 0;
+//         while(i < nums1.length && j < nums2.length){
+//             if(nums1[i] > nums2[j]){
+//                 j++;
+//             }
+//             else if(nums1[i] < nums2[j]){
+//                 i++;
+//             }
+//             else{
+//                 ans[k++] = nums1[i];
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return Arrays.copyOf(ans,k);
+//     }
+// }
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if(nums1.length <= 0 ||nums2.length <= 0 ){
-            return new int[0];
+    int[] freq = new int[1001];
+
+        for (int num : nums1) {
+            freq[num]++;
         }
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int[]ans = new int[nums1.length];
+
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
         int k = 0;
-        int i = 0;
-        int j = 0;
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] > nums2[j]){
-                j++;
-            }
-            else if(nums1[i] < nums2[j]){
-                i++;
-            }
-            else{
-                ans[k++] = nums1[i];
-                i++;
-                j++;
+        for (int num : nums2) {
+            if (freq[num] > 0) {
+                temp[k++] = num;
+                freq[num]--;
             }
         }
-        return Arrays.copyOf(ans,k);
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = temp[i];
+        }
+
+        return ans;    
     }
 }

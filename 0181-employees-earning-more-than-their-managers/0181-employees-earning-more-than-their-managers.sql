8734bc07eac19d4bfdc302847fc563dc0996c340
Karan Sharma <ks7585805@gmail.com>
2026-08-05 15:10:23 +0530

    Time: 362 ms (92.65%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,1 +1,7 @@
-SELECT e1.name As Employee FROM Employee e1,Employee e2 WHERE e1.salary > e2.salary and e1.managerId = e2.id
+-- SELECT e1.name As Employee
+-- FROM Employee e1,Employee e2 
+-- WHERE e1.salary > e2.salary and e1.managerId = e2.id;
+SELECT e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2 ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;

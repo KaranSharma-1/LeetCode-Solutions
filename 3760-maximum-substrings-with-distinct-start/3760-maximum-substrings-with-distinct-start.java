e6cf73bc850f854b36943d0c3cdbae960d10e9fc
Karan Sharma <ks7585805@gmail.com>
2026-08-07 10:35:08 +0530

    Time: 11 ms (72.13%), Space: 47.7 MB (76.84%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDistinct(String s) {
+        int[] hash = new int[26];
+        int count = 0;
+        for(int i = 0; i < s.length();i++){
+            hash[s.charAt(i) - 'a']++;
+            if(hash[s.charAt(i) - 'a']  ==1){
+                count++;
+            }
+        }
+        return count;
+    }
+}

071e8b51c3ee1a10de3c7e63689e2ba127af477a
Karan Sharma <ks7585805@gmail.com>
2026-08-07 10:38:24 +0530

    Time: 6 ms (92.12%), Space: 47.9 MB (57.12%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -3,8 +3,9 @@ class Solution {
         int[] hash = new int[26];
         int count = 0;
         for(int i = 0; i < s.length();i++){
-            hash[s.charAt(i) - 'a']++;
-            if(hash[s.charAt(i) - 'a']  ==1){
+            char ch = s.charAt(i);
+            hash[ch - 'a']++;
+            if(hash[ch -'a']  ==1){
                 count++;
             }
         }

ee1e086ea7d4e9c0b7e4da8d6293f12ef84315b6
Karan Sharma <ks7585805@gmail.com>
2026-08-08 15:47:29 +0530

    Time: 0 ms (100%), Space: 43.4 MB (79.92%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,22 +1,40 @@
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+//         int i = 0;
+//         int j = nums.length - 1;
+//         while(i <= j){
+//             if(nums[i] == val && nums[j]!= val){
+//                 int temp =nums[i];
+//                 nums[i] = nums[j];
+//                 nums[j] = temp;
+//                 j--;
+//                 i++;
+//             }
+//             else if(nums[i] == val && nums[j] == val){
+//                 j--;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         return i;
+//     }
+// }
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int j = nums.length - 1;
-        while(i <= j){
-            if(nums[i] == val && nums[j]!= val){
-                int temp =nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j--;
-                i++;
-            }
-            else if(nums[i] == val && nums[j] == val){
-                j--;
-            }
-            else{
-                i++;
-            }
+        int k =0;
+        int n = nums.length;
+        for(int i = 0; i<n;  i++)
+        {
+          if(nums[i]!= val)
+          {
+            nums[k]=nums[i];
+            k++;
+          }
+
         }
-        return i;
+        return k;
+        
+
     }
 }

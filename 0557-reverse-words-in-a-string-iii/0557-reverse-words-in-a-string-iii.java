6eda884b0513a6f6ac066f50bc5b642c965dc63e
Karan Sharma <ks7585805@gmail.com>
2026-08-10 15:39:09 +0530

    Time: 3 ms (99.65%), Space: 46.6 MB (71.23%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,25 +1,48 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         int j = 0;
+//         StringBuilder str = new StringBuilder();
+//         for(int k = 0;k < s.length();k++){
+//             if(s.charAt(k) == ' '){
+//                 int i = k - 1;
+//                 while( i >= j ){
+//                     str.append(s.charAt(i));
+//                     i--;
+//                 }
+//                 j = k+1;
+//                 str.append(' ');
+//             }
+//              if(k == s.length() - 1){
+//                 int i = k;
+//                 while( i >= j ){
+//                     str.append(s.charAt(i));
+//                     i--;
+//                 }
+//             }
+//         }
+//         return str.toString();
+//     }
+// }
 class Solution {
     public String reverseWords(String s) {
-        int j = 0;
-        StringBuilder str = new StringBuilder();
-        for(int k = 0;k < s.length();k++){
-            if(s.charAt(k) == ' '){
-                int i = k - 1;
-                while( i >= j ){
-                    str.append(s.charAt(i));
-                    i--;
+        char[] arr = s.toCharArray();
+        int start = 0;
+        for (int end = 0; end <= arr.length; end++) {
+            if (end == arr.length || arr[end] == ' ') {
+                int left = start;
+                int right = end - 1;
+                while (left < right) {
+                    char temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                    left++;
+                    right--;
                 }
-                j = k+1;
-                str.append(' ');
-            }
-             if(k == s.length() - 1){
-                int i = k;
-                while( i >= j ){
-                    str.append(s.charAt(i));
-                    i--;
-                }
+
+                start = end + 1;
             }
         }
-        return str.toString();
+
+        return new String(arr);
     }
 }

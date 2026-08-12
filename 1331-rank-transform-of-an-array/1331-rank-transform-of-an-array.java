0f2e41b5d11189010b58d64ff652ec9186b91c79
Karan Sharma <ks7585805@gmail.com>
2026-08-12 07:44:59 +0530

    Time: 29 ms (79.77%), Space: 76.9 MB (38.37%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,21 +1,42 @@
+// class Solution {
+//     public int[] arrayRankTransform(int[] arr) {
+//         int n = arr.length;
+//         if(n == 0)return arr;
+//         int count = 1;
+//         int[]temp = Arrays.copyOf(arr,n);
+//         HashMap<Integer,Integer> rank = new HashMap<>();
+//         Arrays.sort(temp);
+//         for(int i = 0;i < n -1;i++){
+//             if(temp[i]!= temp[i+1])
+//            { 
+//             rank.put(temp[i],count);
+//             count++;}
+//         }
+//         rank.put(temp[n- 1],count);
+//         for(int i = 0;i < n ;i++){
+//             arr[i] = rank.get(arr[i]);
+//         }
+//         return arr;
+//     }
+// }
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;
-        if(n == 0)return arr;
-        int count = 1;
-        int[]temp = Arrays.copyOf(arr,n);
-        HashMap<Integer,Integer> rank = new HashMap<>();
-        Arrays.sort(temp);
-        for(int i = 0;i < n -1;i++){
-            if(temp[i]!= temp[i+1])
-           { 
-            rank.put(temp[i],count);
-            count++;}
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        int r = 1;
+
+        for (int num : sorted) {
+            if (!rank.containsKey(num)) {
+                rank.put(num, r++);
+            }
         }
-        rank.put(temp[n- 1],count);
-        for(int i = 0;i < n ;i++){
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = rank.get(arr[i]);
         }
+
         return arr;
     }
 }

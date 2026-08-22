7b20ff1c269270904a2e6f88106636f6792dddc7
Karan Sharma <ks7585805@gmail.com>
2026-08-22 20:48:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.97%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,11 +1,28 @@
+// class Solution {
+//     public boolean isThree(int n) {
+//         int count = 0;
+//         for(int i = 1; i <= n;i++){
+//             if(n%i == 0){
+//                 count++;
+//             }
+//         }
+//         return count == 3;
+//     }
+// }
 class Solution {
     public boolean isThree(int n) {
-        int count = 0;
-        for(int i = 1; i <= n;i++){
-            if(n%i == 0){
-                count++;
+        int root = (int) Math.sqrt(n);
+
+        if (root * root != n) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= root; i++) {
+            if (root % i == 0) {
+                return false;
             }
         }
-        return count == 3;
+
+        return root > 1;
     }
 }

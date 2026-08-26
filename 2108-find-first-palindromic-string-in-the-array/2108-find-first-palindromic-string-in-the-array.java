dcc5a84fc191ab1a7a390dcf2eaf649ee8211a77
Karan Sharma <ks7585805@gmail.com>
2026-08-26 15:03:28 +0530

    Time: 1 ms (100%), Space: 47.4 MB (68.16%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,11 +1,34 @@
+// class Solution {
+//     public String firstPalindrome(String[] words) {
+//         for(int i = 0; i < words.length;i++){
+//             StringBuilder check = new StringBuilder(words[i]);
+//             if(words[i].equals(check.reverse().toString())){
+//                 return words[i];
+//             }
+//         }
+//         return "";
+//     }
+// }
 class Solution {
     public String firstPalindrome(String[] words) {
-        for(int i = 0; i < words.length;i++){
-            StringBuilder check = new StringBuilder(words[i]);
-            if(words[i].equals(check.reverse().toString())){
-                return words[i];
-            }
+      for (String word : words) {
+        if (isPalindrome(word)) {
+            return word;
         }
-        return "";
+      }  
+      return "";
     }
+    private boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            if (s.charAt(left) != s.charAt(right)) { 
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+    } 
 }

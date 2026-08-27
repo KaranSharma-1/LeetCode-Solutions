9aea96071d007ed62f6697d8613f877e7270e887
Karan Sharma <ks7585805@gmail.com>
2026-08-27 09:48:25 +0530

    Time: 1 ms (99.93%), Space: 43.2 MB (57.45%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,16 +1,34 @@
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         int[] freq = new int[128];
+//         for (char c : s.toCharArray()) {
+//             freq[c ]++;
+//         }
+//         int count = 0;
+//         boolean odd = false;
+//         for (int f : freq) {
+//             if (f % 2 == 0) {
+//                 count += f;
+//             } else {
+//                 count += f - 1;
+//                 odd = true;
+//             }
+//         }
+//         return odd ? count + 1 : count;
+//     }
+// }
 class Solution {
     public int longestPalindrome(String s) {
         int[] freq = new int[128];
         for (char c : s.toCharArray()) {
-            freq[c ]++;
+            freq[c]++;
         }
         int count = 0;
         boolean odd = false;
         for (int f : freq) {
-            if (f % 2 == 0) {
-                count += f;
-            } else {
-                count += f - 1;
+            count += f / 2 * 2;
+
+            if (f % 2 != 0) {
                 odd = true;
             }
         }

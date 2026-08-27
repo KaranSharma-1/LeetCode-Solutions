faee31682170a578431fa1b40112307e6919d62d
Karan Sharma <ks7585805@gmail.com>
2026-08-27 09:46:40 +0530

    Time: 1 ms (99.93%), Space: 43.1 MB (72.64%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];
+        for (char c : s.toCharArray()) {
+            freq[c ]++;
+        }
+        int count = 0;
+        boolean odd = false;
+        for (int f : freq) {
+            if (f % 2 == 0) {
+                count += f;
+            } else {
+                count += f - 1;
+                odd = true;
+            }
+        }
+        return odd ? count + 1 : count;
+    }
+}

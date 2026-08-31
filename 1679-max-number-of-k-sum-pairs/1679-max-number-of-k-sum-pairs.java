9d54e50e287c9fff1b222cad56f4f41c86929003
Karan Sharma <ks7585805@gmail.com>
2026-08-31 15:37:59 +0530

    Time: 217 ms (5.17%), Space: 68.9 MB (80.31%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,19 +1,68 @@
+//hashmap
+// class Solution {
+//     public int maxOperations(int[] nums, int k) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         int count = 0;
+//         for(int i = 0; i< nums.length;i++){
+//             if(map.containsKey(k - nums[i])){
+//                 map.put(k - nums[i],map.getOrDefault(k - nums[i], 0)-1);
+//                 if(map.get(k - nums[i]) == 0){
+//                     map.remove(k - nums[i]);
+//                 }
+//                 count++;
+//             }
+//             else{
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//             }
+//         }
+//         return count;
+//     }
+// }
+//cleaner
+// class Solution {
+//     public int maxOperations(int[] nums, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int count = 0;
+
+//         for (int num : nums) {
+//             int complement = k - num;
+
+//             Integer freq = map.get(complement);
+
+//             if (freq != null) {
+//                 if (freq == 1)
+//                     map.remove(complement);
+//                 else
+//                     map.put(complement, freq - 1);
+
+//                 count++;
+//             } else {
+//                 map.put(num, map.getOrDefault(num, 0) + 1);
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+//two pointer by sorting
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int count = 0;
-        for(int i = 0; i< nums.length;i++){
-            if(map.containsKey(k - nums[i])){
-                map.put(k - nums[i],map.getOrDefault(k - nums[i], 0)-1);
-                if(map.get(k - nums[i]) == 0){
-                    map.remove(k - nums[i]);
-                }
-                count++;
-            }
-            else{
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        int res = 0;
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length-1;
+        while(i < j){
+            int s = nums[i] + nums[j];
+            if(s == k){
+                res++;
+                i++;
+                j--;
+            } else if(s > k){
+                j--;
+            } else {
+                i++;
             }
         }
-        return count;
+        return res;
     }
 }

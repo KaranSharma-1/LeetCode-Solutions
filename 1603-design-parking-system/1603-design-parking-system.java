68c171ef215323ffe7f9a81d5c042c0450962418
Karan Sharma <ks7585805@gmail.com>
2026-09-06 17:59:52 +0530

    Time: 7 ms (100%), Space: 46.8 MB (80.1%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,47 +1,58 @@
-class ParkingSystem {
-    int big;
-    int medium;
-    int small;
-    public ParkingSystem(int big, int medium, int small) {
-        this.big = big;
-        this.medium = medium;
-        this.small = small;
-    }
+// class ParkingSystem {
+//     int big;
+//     int medium;
+//     int small;
+//     public ParkingSystem(int big, int medium, int small) {
+//         this.big = big;
+//         this.medium = medium;
+//         this.small = small;
+//     }
     
+//     public boolean addCar(int carType) {
+//         if(carType == 1){
+//             if(big > 0){
+//                 big--;
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         else if(carType == 2){
+//             if(medium > 0){
+//                 medium--;
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         else if(carType == 3){
+//             if(small > 0){
+//                 small--;
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+class ParkingSystem {
+    private int[] slots;
+
+    public ParkingSystem(int big, int medium, int small) {
+        slots = new int[] {0, big, medium, small};
+    }
+
     public boolean addCar(int carType) {
-        if(carType == 1){
-            if(big > 0){
-                big--;
-                return true;
-            }
-            else{
-                return false;
-            }
+        if (slots[carType] > 0) {
+            slots[carType]--;
+            return true;
         }
-        else if(carType == 2){
-            if(medium > 0){
-                medium--;
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        else if(carType == 3){
-            if(small > 0){
-                small--;
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
+
         return false;
     }
 }
-
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem obj = new ParkingSystem(big, medium, small);
- * boolean param_1 = obj.addCar(carType);
- */

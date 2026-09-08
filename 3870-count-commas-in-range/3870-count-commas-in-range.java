db28372c348aaa9dffeef9aecba068a6c27d43a8
Karan Sharma <ks7585805@gmail.com>
2026-09-08 09:27:37 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (22.48%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countCommas(int n) {
+        if (n < 1000) return 0;
+        if (n < 1000000)
+            return n - 999;
+        return 999000 + 2 * (n - 999999);
+    }
+}

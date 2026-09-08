f76f22bc6d95ce4a8936a7b0db6d968e103ac49a
Karan Sharma <ks7585805@gmail.com>
2026-09-08 09:41:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (50.44%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String common = strs[0];
+        for(int i = 1; i < strs.length;i++){
+            while(!strs[i].startsWith(common)){
+                common = common.substring(0,common.length() - 1);
+            }
+            if(common.length() == 0)return "";
+        }
+        return common;
+    }
+}
